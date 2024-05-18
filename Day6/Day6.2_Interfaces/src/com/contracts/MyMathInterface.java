73f91e3b5dd5744dbc171fd26dc9a71183a7c58a
renuka rathod <rathodrenuka899@gmail.com>
2024-05-18 23:58:16 +0530

    nested class

---
+++ Day6/Day6.2_Interfaces/src/com/contracts/MyMathInterface.java
@@ -0,0 +1,30 @@
+package com.contracts;
+
+public interface MyMathInterface {
+	// i/f contins all abstract methods
+	// by default methods are public abstract
+
+	int add(int i, int j);// public abstract int add(int i,int j)
+
+	int subs(int i, int j);
+	// java 8 feature
+	// default method:methods implemented by i/f
+
+	 default int min(int i,int j) {
+		 return (i<j?i:j);
+	 }
+	
+	
+	default int max(int i, int j) {
+		// implemented methods
+		if (i > j)
+			return i;
+		else
+			return j;
+	}
+}
+
+//Rule(contract): add,subs
+//Can we create instance of i/f ?
+//how to use i/f
+//create provider class and implements Interface
